38f0c44630ef699cfe8d8df9bdbd9e221b410c71
Animesh rastogi <animesh.rastogi54@gmail.com>
2024-08-26 01:43:52 +0530

    Removing Excess Log Lines

--- country.go
+++ country.go
@@ -1,7 +1,6 @@
 package gofaker
 
 import (
-	"fmt"
 	"github.com/rastogiji/go-faker/data"
 	"strings"
 )
@@ -14,22 +13,16 @@ Valid values for s are "africa", "asia", "europe", "americas", "oceania". If s i
 func (f *Faker) Country(s string) string {
 	switch strings.ToLower(s) {
 	case "africa":
-		fmt.Println(len(data.AfricanCountries))
 		return data.AfricanCountries[f.r.Intn(len(data.AfricanCountries))]
 	case "asia":
-		fmt.Println(len(data.AsianCountries))
 		return data.AsianCountries[f.r.Intn(len(data.AsianCountries))]
 	case "europe":
-		fmt.Println(len(data.EuropeanCountries))
 		return data.EuropeanCountries[f.r.Intn(len(data.EuropeanCountries))]
 	case "americas":
-		fmt.Println(len(data.AmericanCountries))
 		return data.AmericanCountries[f.r.Intn(len(data.AmericanCountries))]
 	case "oceania":
-		fmt.Println(len(data.OceanianCountries))
 		return data.OceanianCountries[f.r.Intn(len(data.OceanianCountries))]
 	default:
-		fmt.Println(len(data.AllCountries))
 		return data.AllCountries[f.r.Intn(len(data.AllCountries))]
 	}
 
